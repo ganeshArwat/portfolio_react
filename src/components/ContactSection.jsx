945c905be8c2e0ec7afb396131471addd3b0e903
Ganesh Arwat <ganesharwat123@gmail.com>
2025-05-07 14:52:31 +0530

    contact section responsive

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,31 +1,33 @@
 import { Element } from 'react-scroll'
 function ContactSection() {
   return (
-    <Element name='contact_section' className='h-[250px]'>
-      <div className='flex h-full items-center justify-center rounded-t-[143px] bg-gradient-to-r from-[#8630ca] to-[#732354] text-center text-white'>
-        <div className='w-[45%] pl-[50px] text-3xl font-semibold'>
-          <h3 className='flex flex-wrap items-center justify-end gap-4 sm:justify-start'>
+    <Element name='contact_section' className=''>
+      <div className='flex flex-col items-center justify-center bg-gradient-to-r from-[#8630ca] to-[#732354] text-center text-white md:flex-row md:rounded-t-[143px]'>
+        <div className='md:py-15 w-full px-4 py-14 text-4xl font-semibold md:w-[45%]'>
+          <h3 className='flex flex-col flex-wrap items-center justify-end gap-4 sm:justify-start md:flex-row'>
             Contact me today -
-            <a
-              href='mailto:ganesharwat123@gmail.com'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='text-white no-underline'
-            >
-              <i className='fa-solid fa-envelope'></i>
-            </a>
-            <a
-              href='https://www.linkedin.com/in/ganesh-arwat/'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='text-white no-underline'
-            >
-              <i className='fa-brands fa-linkedin'></i>
-            </a>
+            <span className='flex gap-4'>
+              <a
+                href='mailto:ganesharwat123@gmail.com'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-white no-underline'
+              >
+                <i className='fa-solid fa-envelope'></i>
+              </a>
+              <a
+                href='https://www.linkedin.com/in/ganesh-arwat/'
+                target='_blank'
+                rel='noopener noreferrer'
+                className='text-white no-underline'
+              >
+                <i className='fa-brands fa-linkedin'></i>
+              </a>
+            </span>
           </h3>
         </div>
-        <div className='text-left text-[35px]'>
-          <h2>
+        <div className='md: flex items-center justify-center px-4 text-cnter md:text-left text-[35px]'>
+          <h2 className='md:py-15 md:pt-15 px-4 py-14 '>
             Let's Connect and <br /> Create something Amazing!
           </h2>
         </div>
