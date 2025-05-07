f81e04fa85325613acd20f544be09d4d48982050
Ganesh Arwat <ganesharwat123@gmail.com>
2025-05-07 14:34:37 +0530

    resume section done

--- src/components/ResumeSection.jsx
+++ src/components/ResumeSection.jsx
@@ -4,26 +4,23 @@ function ResumeSection() {
   return (
     <Element
       name='resume_section'
-      className='flex h-[320px] flex-col justify-center gap-5'
+      className='flex flex-col justify-center gap-5 py-20'
     >
-      {/* <section
-        id='resume_section'
-        className='flex h-[320px] flex-col justify-center gap-5'
-      > */}
-      <h2 className='ml-[150px] text-4xl font-bold text-dark'>
+      <h2 className='text-center text-4xl font-bold text-dark'>
         Download My Resume
       </h2>
-      <p className='ml-[100px] text-[17px] font-medium text-gray-800'>
+      <p className='text-center text-[17px] font-medium text-gray-800'>
         Stay updated with latest Projects and Skills
       </p>
-      <a
-        href='/Ganesh_Arwat_Resume.pdf'
-        download
-        className='ml-[150px] inline-block self-start bg-dark px-5 py-2 text-xl text-white no-underline'
-      >
-        Download
-      </a>
-      {/* </section> */}
+      <p className='text-center'>
+        <a
+          href='/Ganesh_Arwat_Resume.pdf'
+          download
+          className='inline-block self-start bg-dark px-5 py-2 text-center text-xl text-white no-underline'
+        >
+          Download
+        </a>
+      </p>
     </Element>
   )
 }
