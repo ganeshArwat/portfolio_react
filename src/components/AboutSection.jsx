bd87dd2e8ed60fd09d16fe23be0cbeed26d6763b
Ganesh Arwat <ganesharwat123@gmail.com>
2025-05-07 13:54:00 +0530

    about section responsive

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -4,14 +4,14 @@ function AboutSection() {
       <div className='flex w-full max-w-6xl flex-col items-center justify-center gap-8 px-4 md:flex-row md:items-start md:gap-12'>
         {/* Heading */}
         <div className='w-full text-left md:w-1/2'>
-          <h2 className='text-xl font-semibold leading-snug text-gray-900 md:text-2xl'>
+          <h2 className='text-xl font-semibold leading-snug text-gray-900 md:text-2xl text-center'>
             Passionate software developer <br />
             with a diverse background and <br />a drive for innovation.
           </h2>
         </div>
 
         {/* Content */}
-        <div className='w-full text-left md:w-1/2'>
+        <div className='w-full text-center md:text-left md:w-1/2'>
           <p className='text-base leading-relaxed text-gray-700 md:text-sm'>
             With over 2 years of industry experience, I have honed my skills in
             web development and software engineering. <br />
